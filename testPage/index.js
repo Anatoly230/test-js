21a85dea2009ecd72b0d58877b19bc6e03db9559
Anatly <maximos225@mail.ru>
2022-04-10 04:03:10 +0300

    игра с регулярными выражениями

--- testPage/index.js
+++ testPage/index.js
@@ -1,3 +1,20 @@
-const firstPar = document.querySelector(".page-text")
+const firstPar = document.querySelector(".page_text");
+const formPhone = document.querySelector(".form_phone");
+const formSubmit = document.querySelector(".form_submit");
 let text = firstPar.textContent;
-console.log(text);
+let rgxp = /[0-9][0-9][0-9]-?[0-9][0-9][0-9]-?[0-9][0-9]-?[0-9][0-9]/gi;
+let testStr = "985-468-7-651";
+
+formPhone.addEventListener("click", function(e){
+if(e.target.className === "form_phone" && formPhone.value === formPhone.ariaLabel){
+  formPhone.value = "";
+}
+})
+
+formSubmit.addEventListener("submit", function(e){
+if(rgxp.test(formPhone.value)){
+console.log("Happynes")}else(
+  console.log("Error")
+)
+})
+console.log(rgxp.test(testStr))
